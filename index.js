b6fbbc90d22c4fa73709d62763e8d9ab103b0271
sumaInovation <136235586+sumaInovation@users.noreply.github.com>
2024-09-14 19:24:01 +0530

    updates index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const app=express();
+require('dotenv').config();
+
+const PORT=process.env.PORT||3000
 
 
 
 
-
-app.listen(3000,()=>console.log("server is running on 3000"));
+app.listen(PORT,()=>console.log(`server is running on Port ${PORT}`));
