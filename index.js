53069f5b61cc45b1099f22adfb27f9c925f1449d
sumaInovation <136235586+sumaInovation@users.noreply.github.com>
2024-09-15 03:54:14 +0530

    adding

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const express=require('express');
+const express = require('express');
 const app = express();
 const session = require('express-session')
 const passport = require('passport')
-const bodyparser=require('body-parser')
-const bcrypt=require('bcryptjs');
+const bodyparser = require('body-parser')
+const bcrypt = require('bcryptjs');
 const LocalStrategy = require('passport-local').Strategy;
 const MongoDBStore = require('connect-mongodb-session')(session);
+require('dotenv').config();
+const PORT=process.env.PORT||3000
 const store = new MongoDBStore({
-    uri: 'mongodb+srv://suma:123@cluster0.wxldekr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    uri: process.env.MONGODB_URL,
     collection: 'mySessions'
-  });
+});
 
-  // Catch errors
-store.on('error', function(error) {
+// Catch errors
+store.on('error', function (error) {
     console.log(error);
-  });
+});
 
 
-let user={};
+let user = {};
 //midlleware
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyparser.json());
@@ -34,41 +36,58 @@ app.use(session({
 
 passport.use(new LocalStrategy(
     function (username, password, done) {
-        if(username!=user.username)return done(null, false, { message: 'Incorrect username.' });
-           bcrypt.compare(password.toString(), user.password, (err, result) => {
+        if (username != user.username) return done(null, false, { message: 'Incorrect username.' });
+        bcrypt.compare(password.toString(), user.password, (err, result) => {
             if (err) return done(err);
             if (!result) return done(null, false, { message: 'Incorrect password.' });
             return done(null, user);
-          });
+        });
 
-}
+    }
 ));
 app.use(passport.initialize());
 app.use(passport.session());
 
 
 //Routes
-app.post('/register',async(req,res)=>{
-    const {username,password}=req.body
-    user.username=username;
-    user.password=await bcrypt.hash(password.toString(),10)
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body
+    user.username = username;
+    user.password = await bcrypt.hash(password.toString(), 10)
     res.send("registration succuss");
-     
+
 })
 
-app.post('/login', 
+app.post('/login',
     passport.authenticate('local', { failureRedirect: '/login' }),
-    function(req, res) {
-      res.send('success');
+    function (req, res) {
+        res.send('success');
     });
-app.get('/login',(req,res)=>{
+app.get('/login', (req, res) => {
     res.send("HOME");
 })
-    passport.serializeUser(function(user, done) {
-        done(null, user);
-      });
-      
-      passport.deserializeUser(function(user, done) {
-        done(null, user);
-      });
-app.listen(3000,()=>{});
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+
+
+app.get('/welcome', (req, res) => {
+    console.log("Herer")
+    if (!req.isAuthenticated()) {
+    return res.redirect('/login');
+    }
+    res.send(`Welcome ${req.user.username} <a href="/test">Logout</a>`);
+});
+
+
+app.post('/logout', (req, res) => {
+    req.logout(()=>{
+        console.log("Succuess full logout")
+    });
+    res.redirect('/login');
+});
+app.listen(PORT, () => {console.log(`Server is running on PORT:${PORT}`) });
