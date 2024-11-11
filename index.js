41df6b7023f5815058e0427ca62832e32002c4fc
sumaautomationLK <sumanga0000@gmail.com>
2024-11-12 02:16:29 +0530

    edited

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = require('http').createServer(app);
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({server:server});
-
+var dataincomming;
 wss.on('connection', function connection(ws) {
     //**************************************** */
   console.log('A new client Connected!');
@@ -14,6 +14,7 @@ wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
     ws.send("you pass value is:"+message);
+    dataincomming=message;
    // Share message all connected client
    wss.clients.forEach(function each(client) {
     if (client !== ws && client.readyState === WebSocket.OPEN) {
@@ -31,6 +32,7 @@ wss.on('connection', function connection(ws) {
  
 });
 
-app.get('/', (req, res) => res.send('Hello World sumanga!'))
+app.get('/', (req, res) => res.send(
+  `incomming data:${dataincomming}`))
 
 server.listen(3000, () => console.log(`Lisening on port :3000`))       
